a1a148178f4c3c18531bc61e4f504ae5c045c207
Restyled.io <commits@restyled.io>
2022-01-31 04:11:24 +0000

    Restyled by black

--- polygoner.py
+++ polygoner.py
@@ -1,9 +1,26 @@
-number_names = ("zero-invalid", "one-invalid", "two-invalid", "tri", "tetra",
-                "penta", "hexa", "hepta", "octa", "nona", "deca", "hendeca", "dodeca", "Triskaideca", "Tetradeca",)
+number_names = (
+    "zero-invalid",
+    "one-invalid",
+    "two-invalid",
+    "tri",
+    "tetra",
+    "penta",
+    "hexa",
+    "hepta",
+    "octa",
+    "nona",
+    "deca",
+    "hendeca",
+    "dodeca",
+    "Triskaideca",
+    "Tetradeca",
+)
 
-print("Welcome to the Polygoner. Give it a number of sides, between 3-12 and get the polygon's name \n Made by Qubisted | Advaith Sai")
+print(
+    "Welcome to the Polygoner. Give it a number of sides, between 3-12 and get the polygon's name \n Made by Qubisted | Advaith Sai"
+)
 
 number = input("How many sides does your mystery polygon have? > ")
 
 answer = number_names[int(number)]
-print("It is a/an "+answer+"gon!")
+print("It is a/an " + answer + "gon!")
